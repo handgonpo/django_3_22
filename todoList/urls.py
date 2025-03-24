bc4f320eb2bc90cef25488ba610e17b0e019bdb9
Youjung <handgonpo@naver.com>
2025-03-25 01:14:37 +0900

    [feat] - name 필드에 name이 포함된 객체를 필터링

--- todoList/urls.py
+++ todoList/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
-from todoList import views
+from django.urls import path, include
+from .views import *
 
+# 127.0.0.1:8000/todoList/제목검색어
 urlpatterns = [
-    path('', views.todo_List), # 127.0.0.1:8000/todo_List/
+   path("<str:name>/", todo_detail), # 127.0.0.1:8000/todoList/<str:name>/ name = 공부를 찾아서 리턴
 ]
+
+# urls.py (라우팅) -> views.py(로직) -> ~~.html(템플릿)
--- todoList/views.py
+++ todoList/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from .models import TodoList
+from django.http import HttpResponse
 
+
+# path("update/<int:pk>/", TodoUpdateView.as_view()), 
+# path("create/", TodoCreateView.as_view()),
+# path("list/", TodoListView.as_view()), 
+# path("<int:pk>/", TodoDetailView.as_view()), 
+# path("<str:name>/", todo_detail),  
 
 def todo_List(request):
     todos = TodoList.objects.all()
+
+    # search = request.GET.get("search")
+    # if search:
+    #     todos = todos.filter(name__icontains=search)
+
     return render(request, "todoList/todo_List.html", {"todos" : todos})
     #render(request, template_name, context=None, content_type=None, status=None, using=None)
 ''''
@@ -12,3 +24,15 @@ request: 클라이언트로부터 받은 요청 객체
 {"todos": todos}: 템플릿에 전달할 context (딕셔너리 형태)
 즉, “요청 → 템플릿 경로 → 전달할 데이터(context)”
 '''
+
+def todo_detail(request, name):
+    # todo = Todo.objects.get(pk=pk) == Todo.objects.get(id=pk)
+    # try:
+    #     todo = Todo.objects.get(id=pk) # -> Todo Object
+    # except Todo.DoesNotExist: # 예외처리
+    #     return HttpResponse("없는 페이지입니다.", status=404)
+    todos = TodoList.objects.filter(name__icontains=name) # name__icontains=name : name 필드에 name이 포함된 객체를 필터링
+    if not todos.exists():
+        return HttpResponse("없는 페이지입니다.", status=404)
+    
+    return render(request, "todoList/todo_List.html", {"todos": todos, "keyword": name})
