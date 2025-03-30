8b4ce03bb2c0c999aebc9e18af8716d31fe24741
Youjung <handgonpo@naver.com>
2025-03-30 14:56:24 +0900

    [feat] - update.html - edit list,create,detail.html - add X-CSRFToken head.html - path_update urls.py

--- todoList/urls.py
+++ todoList/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 # 127.0.0.1:8000/todoList/제목검색어
 urlpatterns = [
+   path("update/<int:pk>/", TodoUpdateView.as_view()), # 127.0.0.1:8000/todoList/update/<int:pk>
    path('list/', todo_List, name = 'todo_List'), # 127.0.0.1:8000/todoList/list/ 경로변경
    path("create/", TodoCreateView.as_view()),# 127.0.0.1:8000/todoList/create/
    path("detail/<int:pk>/", TodoDetailView.as_view()), # 127.0.0.1:8000/todoList/detail/<str:name>/ name = 공부를 찾아서 리턴 
